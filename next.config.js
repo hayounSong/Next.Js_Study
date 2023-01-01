e8d978b5da1c9e1d236b93d2972b9b79fbd3fcbb
hayoun <fishingest@kookmin.ac.kr>
2023-01-02 01:47:03 +0900

    NextJS의 redirect, rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
-/** @type {import('next').NextConfig} */
+const API_KEY = 'd694e0e93cf7e09038bd1dd7160e3088';
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/contact',
+        destination: '/form',
+        permanent: false,
+      },
+      {
+        source: '/old-blog/:path',
+        destination: '/new-sexy-blog/:path',
+        permanent: false,
+        //이런식으로 패턴 인식도 가능하다!
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+    //rewrites는, 유저를 redirect 시키긴하지만, url은 변하지 않는다!
+  },
 };
 
 module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [movies, setMovies] = useState<any[]>([]);
   useEffect(() => {
     (async () => {
-      const { results } = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
+      const { results } = await (await fetch('/api/movies')).json();
       setMovies(results);
     })();
   }, []);
