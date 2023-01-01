e06e79fc9ddcadf06ba9ad90d9bd8db1e152bd47
hayoun <fishingest@kookmin.ac.kr>
2023-01-02 01:31:40 +0900

    영화 DB API 가져오기

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,13 +6,41 @@ export default function NavBar() {
   //현재 있는 페이지의 라우터 정보를 가져올 수 있다.
   return (
     <nav>
-      <Link href="/">
-        <span className={router.pathname === '/' ? 'active' : ''}>Home</span>
-      </Link>
-
-      <Link href="/about">
-        <span className={router.pathname === '/about' ? 'active' : ''}>About</span>
-      </Link>
+      <img src="/vercel.svg" />
+      <div>
+        <Link href="/">
+          <div className={router.pathname === '/' ? 'active' : ''}>Home</div>
+        </Link>
+        <Link href="/about">
+          <div className={router.pathname === '/about' ? 'active' : ''}>About</div>
+        </Link>
+      </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav div {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
     </nav>
   );
 }
