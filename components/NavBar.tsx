87a0f5f34138e7c3e441607d7579473e3a2e9c54
hayoun <fishingest@kookmin.ac.kr>
2023-01-01 20:00:49 +0900

    Styles JSX

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,19 +1,26 @@
 import Link from 'next/link';
 import { NextRouter, useRouter } from 'next/router';
 import React from 'react';
-import styles from './NavBar.module.css';
 export default function NavBar() {
   const router: NextRouter = useRouter();
   //현재 있는 페이지의 라우터 정보를 가져올 수 있다.
   return (
     <nav>
       <Link href="/">
-        <span className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>Home</span>
+        <span className={router.pathname === '/' ? 'active' : ''}>Home</span>
       </Link>
 
       <Link href="/about">
-        <span className={`${styles.link} ${router.pathname === '/about' ? styles.active : ''}`}>About</span>
+        <span className={router.pathname === '/about' ? 'active' : ''}>About</span>
       </Link>
+      <style tsx>{`
+        nav {
+          background-color: tomato;
+        }
+        .active {
+          color: blue;
+        }
+      `}</style>
     </nav>
   );
 }
